8077c6c3a9ebcfb9c59e744a9aaf5d5bb95ea3a6
Andry Sysak <andrijsisae@gmail.com>
2025-09-06 15:34:40 +0300

    #25: Add refresh of next steps on change event

--- force-app/main/default/lwc/reviewProcessViewer/reviewProcessViewer.js
+++ force-app/main/default/lwc/reviewProcessViewer/reviewProcessViewer.js
@@ -1,6 +1,8 @@
 import { LightningElement, track, api, wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
+import { getRecord, notifyRecordUpdateAvailable } from 'lightning/uiRecordApi';
+import { subscribe, unsubscribe, onError } from 'lightning/empApi';
 import { showToast, isEmpty } from 'c/utils';
+import { refreshApex } from '@salesforce/apex';
 
 import getNextStep from '@salesforce/apex/ReviewProcessController.getReviewProcessNextStep';
 import TOTAL_REVIEWABLE_FIELD from '@salesforce/schema/ReviewProcess__c.TotalReviewableRecords__c';
@@ -24,13 +26,57 @@ export default class ReviewProcessViewer extends LightningElement {
                 this.totalNumberOfRecords = totalRecords;
             }
         }
-    }
-
-    @wire(getNextStep, { reviewProcessId: '$recordId' })
-    nextStepHandler({ error, data }) {
-        if (data) {
-            this.nextStep = data.name;
-            this.nextStepStyle = data.isReady ? 'color: #6cc230;' : 'color: #c03030;';
+        if(error) {
+          console.error(error);
         }
     }
+    nextStepResult;
+
+    @wire(getNextStep, { reviewProcessId: '$recordId' })
+    nextStepHandler(value) {
+        this.nextStepResult = value;
+        if (value.data) {
+            this.nextStep = value.data.name;
+            this.nextStepStyle = value.data.isReady ? 'color: #6cc230;' : 'color: #c03030;';
+        }
+    }
+
+    channelName = '/data/ReviewProcess__ChangeEvent';
+    subscription = {};
+
+    connectedCallback() {
+        this.registerErrorListener();
+        this.registerSubscribe();
+      }
+    
+      disconnectedCallback() {
+        unsubscribe(this.subscription, () => console.log('Unsubscribed to change events.'));
+      }
+    
+      registerErrorListener() {
+        onError(error => {
+          console.error('Salesforce error', JSON.stringify(error));
+        });
+      }
+    
+      registerSubscribe() {
+        const changeEventCallback = changeEvent => {
+          this.processChangeEvent(changeEvent);
+        };
+    
+        subscribe(this.channelName, -1, changeEventCallback).then(subscription => {
+          this.subscription = subscription;
+        });
+      }
+    
+      processChangeEvent(changeEvent) {
+        try {
+          const recordIds = changeEvent.data.payload.ChangeEventHeader.recordIds;
+          if (recordIds.includes(this.recordId)) {
+            refreshApex(this.nextStepResult);
+          }
+        } catch (err) {
+          console.error(err);
+        }
+      }
 }
