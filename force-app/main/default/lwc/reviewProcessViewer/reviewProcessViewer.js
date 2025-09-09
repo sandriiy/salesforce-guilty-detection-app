081fd03cf1af9b6f1337c2d4f01f78d93c94328f
Andry Sysak <44209849+Endry@users.noreply.github.com>
2025-09-10 01:29:58 +0300

    #25: Review Process Next Step Real-Time Update + Toast Messages (#35)
    * #25: Add refresh of next steps on change event
    * #25: Add toasts and fixes
    * #25: Move message definitions to controller

--- force-app/main/default/lwc/reviewProcessViewer/reviewProcessViewer.js
+++ force-app/main/default/lwc/reviewProcessViewer/reviewProcessViewer.js
@@ -1,11 +1,14 @@
 import { LightningElement, track, api, wire } from 'lwc';
-import { getRecord } from 'lightning/uiRecordApi';
+import { getRecord, notifyRecordUpdateAvailable } from 'lightning/uiRecordApi';
+import { subscribe, unsubscribe, onError } from 'lightning/empApi';
 import { showToast, isEmpty } from 'c/utils';
+import { refreshApex } from '@salesforce/apex';
 
 import getNextStep from '@salesforce/apex/ReviewProcessController.getReviewProcessNextStep';
 import TOTAL_REVIEWABLE_FIELD from '@salesforce/schema/ReviewProcess__c.TotalReviewableRecords__c';
 
 const DEFAULT_EMPTY = 'N/A';
+const CHANGE_EVENT_CHANNEL = '/data/ReviewProcess__ChangeEvent';
 const FIELDS = [TOTAL_REVIEWABLE_FIELD];
 export default class ReviewProcessViewer extends LightningElement {
     @api recordId;
@@ -15,6 +18,11 @@ export default class ReviewProcessViewer extends LightningElement {
     @track totalNumberOfReviewers = DEFAULT_EMPTY
     @track nextStep = DEFAULT_EMPTY
     @track nextStepStyle;
+    nextStepResult;
+    messageType;
+    helpText = DEFAULT_EMPTY;
+    channelName = CHANGE_EVENT_CHANNEL;
+    subscription = {};
 
     @wire(getRecord, { recordId: '$recordId', fields: FIELDS })
     recordHandler({ error, data }) {
@@ -24,13 +32,71 @@ export default class ReviewProcessViewer extends LightningElement {
                 this.totalNumberOfRecords = totalRecords;
             }
         }
+        else if (error) {
+            showToast(
+                this,
+                'Failed to display summary information.',
+                'Please contact your System Administrator',
+                'error'
+            );
+        }
     }
 
     @wire(getNextStep, { reviewProcessId: '$recordId' })
-    nextStepHandler({ error, data }) {
-        if (data) {
-            this.nextStep = data.name;
-            this.nextStepStyle = data.isReady ? 'color: #6cc230;' : 'color: #c03030;';
+    nextStepHandler(value) {
+        this.nextStepResult = value;
+        if (value.data) {
+            this.nextStep = value.data.name;
+            this.messageType = value.data.messageType;
+            this.helpText = value.data.helpText;
+            this.nextStepStyle = value.data.isReady ? 'color: #6cc230;' : 'color: #c03030;';
         }
+    }
+
+    connectedCallback() {
+        this.registerErrorListener();
+        this.registerSubscribe();
+    }
+
+    disconnectedCallback() {
+        unsubscribe(this.subscription, () => {
+            console.log('Unsubscribed to change events.'); //TODO: Add meaningful log/toast message.
+        });
+    }
+
+    registerErrorListener() {
+        onError(error => {
+            console.error('Salesforce error', JSON.stringify(error)); //TODO: Add meaningful log/toast message.
+        });
+    }
+
+    registerSubscribe() {
+        const changeEventCallback = changeEvent => {
+            this.processChangeEvent(changeEvent);
+        };
+
+        subscribe(this.channelName, -1, changeEventCallback).then(subscription => {
+            this.subscription = subscription;
+        });
+    }
+
+    processChangeEvent(changeEvent) {
+        try {
+            const recordIds = changeEvent.data.payload.ChangeEventHeader.recordIds;
+            if (recordIds.includes(this.recordId)) {
+                refreshApex(this.nextStepResult);
+            }
+        } catch (err) {
+            console.error(err); //TODO: Add meaningful log/toast message.
+        }
+    }
+
+    handleNextStepClick() {
+        showToast(
+            this,
+            this.nextStep,
+            this.helpText,
+            this.messageType
+        );
     }
 }
