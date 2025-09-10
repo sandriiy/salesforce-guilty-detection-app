9498632dcd5b7bebf99515af98c242439a240b45
Andrii Sukhetskyi <ansukhetskyi@gmail.com>
2025-09-10 18:34:10 -0400

    #27: Expand the Functionality of the Reviewer Component

--- force-app/main/default/lwc/reviewerFeedbackRecordModal/reviewerFeedbackRecordModal.js
+++ force-app/main/default/lwc/reviewerFeedbackRecordModal/reviewerFeedbackRecordModal.js
@@ -1,5 +1,6 @@
 import { api, track } from 'lwc';
 import LightningModal from 'lightning/modal';
+import { showToast } from 'c/utils';
 
 import getReviewFeedbackWithItems from '@salesforce/apex/GuiltyReviewerController.getReviewFeedbackWithItemsById';
 import submitReviewFeedback from '@salesforce/apex/GuiltyReviewerController.submitReviewFeedback';
@@ -17,14 +18,20 @@ export default class ReviewerFeedbackRecordModal extends LightningModal {
         this.isLoading = true;
         await getReviewFeedbackWithItems({ reviewFeedbackId: this.feedbackId })
             .then(result => {
-                console.log('HERE === ');
-                console.log(result);
                 this.feedbackRecord = result.reviewFeedback;
                 this.feedbackFields = result.items;
+				this.initialization();
             })
             .catch(error => {
-                // toast
                 console.log(error);
+				showToast(
+					this,
+					'Unable to retrieve information about Review Process',
+					'Please reload the page or try again later',
+					'error'
+				);
+
+				this.close();
             })
             .finally(() => {
                 this.isLoading = false;
@@ -51,10 +58,8 @@ export default class ReviewerFeedbackRecordModal extends LightningModal {
         const value = event.detail.value;
 
         this.feedbackFields = this.feedbackFields.map(item => {
-            return item.id === feedbackFieldId ? { ...item, validation: value } : item;
+            return item.id === feedbackFieldId ? { ...item, assessment: value } : item;
         });
-
-        console.log(JSON.stringify(this.feedbackFields));
     }
 
     handleFieldCommentChange(event) {
@@ -64,8 +69,6 @@ export default class ReviewerFeedbackRecordModal extends LightningModal {
         this.feedbackFields = this.feedbackFields.map(item => {
             return item.id === feedbackFieldId ? { ...item, comment: value } : item;
         });
-
-        console.log(JSON.stringify(this.feedbackFields));
     }
 
     handleGeneralCommentChange(event) {
@@ -77,11 +80,21 @@ export default class ReviewerFeedbackRecordModal extends LightningModal {
         this.isLoading = true;
         await saveReviewFeedback({ reviewFeedbackDTO: this.buildReviewFeedbackDTO })
             .then(() => {
-                // success message toast
+                showToast(
+					this,
+					'Your feedback has been successfully saved',
+					'Note: It\’s not submitted yet, you can keep working on it anytime',
+					'success'
+				);
             })
             .catch(error => {
-                // toast
-                console.log(error);
+				console.log(error);
+                showToast(
+					this,
+					'Unable To Save Your Review',
+					'For more information, contact your System Administrator',
+					'error'
+				);
             })
             .finally(() => {
                 this.isLoading = false;
@@ -95,14 +108,23 @@ export default class ReviewerFeedbackRecordModal extends LightningModal {
 
         const isValid = this.validateFeedbackFields();
         if (isValid) {
-            console.log('a');
             await submitReviewFeedback({ reviewFeedbackDTO: this.buildReviewFeedbackDTO })
                 .then(() => {
-                    // success message toast
+                    showToast(
+						this,
+						'Your feedback has been successfully submitted',
+						'This feedback is complete and requires no further follow-up',
+						'success'
+					);
                 })    
                 .catch(error => {
-                    // toast
-                    console.log(error);
+					console.log(error);
+                    showToast(
+						this,
+						'Unable To Submit Your Review',
+						'For more information, contact your System Administrator',
+						'error'
+					);
                 });
 
             this.close('submit');
@@ -117,14 +139,12 @@ export default class ReviewerFeedbackRecordModal extends LightningModal {
 
         this.feedbackFields.forEach(field => {
             const radioGroup = this.template.querySelector(`lightning-radio-group[data-id="${field.id}"]`);
-
-            if (!field.validation || field.validation.trim() === '') {
+            if (!field.assessment || field.assessment.trim() === '') {
                 isValid = false;
 
                 if (radioGroup) {
                     radioGroup.setCustomValidity('Your assessment is required');
                     radioGroup.reportValidity();
-
                     if (!firstInvalid) {
                         firstInvalid = radioGroup;
                     }
@@ -142,7 +162,12 @@ export default class ReviewerFeedbackRecordModal extends LightningModal {
         }
 
         if (!isValid) {
-            // toast message
+            showToast(
+				this,
+				'Please fix the errors before proceeding',
+				'',
+				'warning'
+			);
         }
 
         return isValid;
@@ -154,9 +179,15 @@ export default class ReviewerFeedbackRecordModal extends LightningModal {
             items: this.feedbackFields.map(field => ({
                 id: field.id,
                 comment: field.comment || '',
-                assessment: field.validation || ''
+                assessment: field.assessment || ''
             }))
         };
     }
+
+	initialization() {
+		if (this.feedbackRecord?.AdditionalComment__c) {
+			this.generalComment = this.feedbackRecord?.AdditionalComment__c;
+		}
+	}
 
 }
